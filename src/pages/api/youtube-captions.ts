5c5a0c74f45275e91611ca0fa7ff7dfbce53d63c
Nguyen Hau <conghaunguyen909@gmail.com>
2025-05-13 18:10:08 +0700

    fix conflict

--- src/pages/api/youtube-captions.ts
+++ src/pages/api/youtube-captions.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getSubtitles } from "youtube-captions-scraper";
 
+export const runtime = "edge";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
