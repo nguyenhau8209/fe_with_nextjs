37832841325ee068f450a5eed52e5aa1d2b6bed3
Nguyen Hau <conghaunguyen909@gmail.com>
2025-05-13 18:56:00 +0700

    fix Edge Runtime

--- src/pages/api/youtube-captions.ts
+++ src/pages/api/youtube-captions.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getSubtitles } from "youtube-captions-scraper";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
